725466bc2704928fe0aa0d7e65073a1246c80e4f
pandurang sadgir <psadgir111@gmail.com>
2023-05-03 22:24:48 +0530

    create the button componet and use it into root componet App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import MyButton from './components/Buttons/button';
+
 
 function App() {
   return (
-    <div className="heading">
-      <header>
-        <h1 >Welcome to React js</h1>
-      </header>
-    </div>
+    <><div className="heading">
+       <header>
+        <h1>Welcome to React js</h1>
+       </header>
+      </div>
+      <MyButton />
+      <MyButton />
+      <MyButton />
+      <MyButton />
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Buttons/button.js
@@ -0,0 +1,8 @@
+import './button.css';
+function MyButton (){
+    return (
+        <button className="button"> Click me</button>
+    )
+}
+
+export default MyButton;
